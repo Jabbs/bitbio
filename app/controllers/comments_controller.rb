8f3a941913ddddfc8a831c4e28d676189b7a522d
Peter Jabbour <petejabbour1@gmail.com>
2013-05-20 08:13:11 -0500

    comment styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,12 @@
 class CommentsController < ApplicationController
-  
+  def create
+    @comment = Comment.new(params[:comment])
+    @project = Project.find(params[:comment][:project_id])
+    @comments = @project.comments
+    if @comment.save
+      redirect_to project_path(@project), notice: 'Your comment has been posted'
+    else
+      render template: 'projects/show'
+    end
+  end
 end
