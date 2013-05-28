8b1f9ad0e96baa1cf210a64edb3eda0627193302
Peter Jabbour <petejabbour1@gmail.com>
2013-05-28 13:47:29 -0500

    some more changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -41,25 +41,26 @@ class Project < ActiveRecord::Base
   
   TAGS = ["abiogenesis", "anatomy", "antibiotics", 
     "astrobiology", "bacteriology", "biochemistry", "biophysics", "biotechnology", "botany", 
-    "cardiology", "cell-biology", "cell-culture", "cloning", "cytogenetics", "cytology", 
-    "developmental-biology", "dna-sequencing", "ecoli", "ecology", "electrophysiology", 
+    "cardiology", "cell-biology", "cell-culturing", "cloning", "cytogenetics", "cytology", 
+    "developmental-biology", "dna-sequencing", "ecology", "electrophysiology", 
     "embryology", "endocrinology", "entomology", "epidemiology", "epigenetics", 
     "ethology", "gel-electrophoresis", "gene-annotation", "gene-expression", 
-    "gene-regulation", "genetics", "genomics", "gwas", "hematology", "histology", "hiv", 
+    "gene-regulation", "genetics", "genomics", "hematology", "histology", 
     "human-genetics", "human-genome", "ichthyology", "imaging", "immunology", 
     "marine-biology", "medicinal-chemistry", "metabolism", "microbiology", "microscopy", 
     "molecular-biology", "mycology", "neuroanatomy", "neurology", "neuroscience", 
     "neurotransmission", "next-generation-sequencing", "nutrition", "ornithology", 
     "palaeontology", "parasitology", "pathology", "pharmacology", "phylogenetics", 
     "physiology", "plant-anatomy", "plant-physiology", "population-biology", 
-    "protein-binding", "protein-folding", "protocol", "psychology", 
+    "protein-binding", "protein-folding", "psychology", 
     "rna-sequencing", "sequence-analysis", "software-development", "species-identification", "staining", 
     "statistics", "stem-cells", "structural-biology", "stynthetic-biology", "stystems-biology", 
-    "taxonomy", "telomere", "theoretical-biology", "toxicology", 
+    "taxonomy", "theoretical-biology", "toxicology", 
     "vaccination", "virology", "vitamins", "zoology"]
   
   # callbacks
   after_validation :move_friendly_id_error_to_name
+  before_save :strip_inputs
   
   # validations
   validates :description, presence: true, length: { minimum: 90, maximum: 2000 }
@@ -75,6 +76,11 @@ class Project < ActiveRecord::Base
   has_many :messages
   accepts_nested_attributes_for :instruments, allow_destroy: true
   
+  def strip_inputs
+    self.name = self.name.strip
+    self.science_type = self.science_type.strip
+  end
+  
   def days_til_start
     (self.start_date - Date.today).to_i
   end
@@ -83,7 +89,7 @@ class Project < ActiveRecord::Base
     errors.add :name, *errors.delete(:friendly_id) if errors[:friendly_id].present?
   end
   
-  def self.search(keyword=nil, from=nil, to=nil, location=nil, science=nil, instrument=nil)
+  def self.search(keyword=nil, from=nil, to=nil, location=nil, science=nil, tag=nil)
     projects = self.scoped
     unless keyword.blank? || keyword == nil
       projects = projects.where("name LIKE ? OR description LIKE ? OR science_type LIKE ?", "%#{keyword}%","%#{keyword}%","%#{keyword}%")
@@ -94,11 +100,12 @@ class Project < ActiveRecord::Base
     unless location.blank? || location == nil
       projects = projects.joins(:user).where(users: {continent: location})
     end
-    unless instrument.blank? || instrument == nil
-      projects = projects.joins(:instruments).where(instruments: {alias: instrument})
+    unless tag.blank? || tag == nil
+      # projects = projects.joins(:instruments).where(instruments: {alias: instrument})
+      projects = projects.where(tag: tag)
     end
     unless from == nil || to == nil || from.blank? || to.blank?
-      projects = projects.where(start_date: ( from..to) )
+      projects = projects.where(created_at: ( from..to) )
     end
     projects
   end
--- lib/tasks/sample_date.rake
+++ lib/tasks/sample_date.rake
@@ -7,6 +7,7 @@ namespace :db do
                    account_type: 'Researcher', organization: 'The Stowers Institute', phone: '785-550-8670',
                    bio: Faker::Lorem.paragraph, address: "3049 W Fullerton #1", city: "Chicago",
                    state: "IL", zip: "60647", country: "United States of America")
+      user.verified = true
       user.save!
     end
     
@@ -16,18 +17,20 @@ namespace :db do
                     organization: Faker::Company.name, phone: Faker::PhoneNumber.phone_number, bio: Faker::Lorem.paragraph,
                     address: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state_abbr,
                     zip: Faker::Address.zip_code, country: Ravibhim::Continents::COUNTRIES.shuffle.first)
+      user.verified = true
       user.save!
     end
     
-    20.times do
+    100.times do
       name = ['Human', 'Rat', 'Fly', 'Mouse', 'Zebra Fish', 'Yeast', 'Lizard'].shuffle.first + " " + ['DNA', 'Genome', 'RNA', 'Ribosome', 'Histone'].shuffle.first + " " + ['Sequencing', 'Analysis', 'Experiment', 'Research', 'Study'].shuffle.first + " " + ('a'..'z').to_a.shuffle.first(8).join
       description = Faker::Lorem.paragraph + ' ' + Faker::Lorem.paragraph + ' ' + Faker::Lorem.paragraph + ' ' + Faker::Lorem.paragraph
       science_type = ['DNA Sequencing', 'RNA Sequencing', 'Chip-SEQ', 'Micro Array', 'Next-gen Sequencing',
                       'Microscopy', 'Cytometry'].shuffle.first
-      service_need = ['Science', 'Services', 'Science + Services', 'Data Analysis', 'Clinical Studies', 'Research Validation'].shuffle.first
+      service_need = Project::SERVICE_NEEDS.shuffle.first
+      tag = Project::TAGS.shuffle.first
       start_date = rand(300).days.from_now.to_date
       project = Project.new(name: name, description: description, science_type: science_type, service_need: service_need,
-                            start_date: start_date)
+                            start_date: start_date, tag: tag)
       project.user_id = User.all.shuffle.first.id
       
       x = rand(1..3)
@@ -39,9 +42,19 @@ namespace :db do
       project.save!
     end
     
-    80.times do
+    200.times do
       Comment.create!(content: Faker::Lorem.paragraph, project_id: Project.all.shuffle.first.id,
                       user_id: User.all.shuffle.first.id)
+      
+    end
+    
+    80.times do
+      project = Project.all.shuffle.first
+      subject = "Project - #{project.name}"
+      Message.create!(content: Faker::Lorem.paragraph, project_id: Project.all.shuffle.first.id,
+                      sender_id: User.where(verified: true).shuffle.first.id,
+                      receiver_id: project.user.id, project_id: project.id,
+                      subject: subject)
       
     end
   end
