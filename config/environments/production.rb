ac60ded69fc1a5e30a7f41ac06dfce2f606aea7e
Peter Jabbour <petejabbour1@gmail.com>
2013-06-02 23:11:01 -0500

    assets compiling error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Bitbio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
+  # assets precompiling error fix - reference here
+  # http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
+  config.assets.initialize_on_precompile = false
+  
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
   
