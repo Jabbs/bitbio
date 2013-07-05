eeb43c716eb284ecb90c6254cc6de99dacb9c8c5
Peter Jabbour <petejabbour1@gmail.com>
2013-07-05 08:42:10 -0500

    more event styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -87,4 +87,6 @@ module ApplicationHelper
     return
   end
   
+  
+  
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,7 @@
 module EventsHelper
+  
+  def date_time_info(event)
+    event.start_date.strftime('%b %e') + ", " + event.start_date.strftime('%l:%M%P') + " -- " + event.end_date.strftime('%b %e') + ", " + event.end_date.strftime('%l:%M%P')
+  end
+  
 end
