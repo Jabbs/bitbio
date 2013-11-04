75008aad8dbc2d67c0edbf809fd15ef5a116b915
Peter Jabbour <petejabbour1@gmail.com>
2013-11-04 11:54:14 -0600

    another fix

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -2,10 +2,10 @@ module FacilitiesHelper
   
   def city_state_zip_country(location)
     x = []
-    x << location.city if !location.city.empty?
-    x << location.state if !location.state.empty?
-    x << location.zip if !location.zip.empty?
-    x << location.country if !location.country.empty?
+    x << location.city if location.city && !location.city.empty?
+    x << location.state if location.state && !location.state.empty?
+    x << location.zip if location.zip && !location.zip.empty?
+    x << location.country if location.country && !location.country.empty?
     x.join(', ')
   end
 end
