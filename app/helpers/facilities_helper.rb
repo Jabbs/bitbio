56600bb2d1cff1ffa5e44cc588d1c83c12a13f34
Peter Jabbour <petejabbour1@gmail.com>
2013-11-04 11:42:37 -0600

    changed info location show

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -2,10 +2,10 @@ module FacilitiesHelper
   
   def city_state_zip_country(location)
     x = []
-    x << location.city if location.city
-    x << location.state if location.state
-    x << location.zip if location.zip
-    x << location.country if location.country
+    x << location.city if !location.city.empty?
+    x << location.state if !location.state.empty?
+    x << location.zip if !location.zip.empty?
+    x << location.country if !location.country.empty?
     x.join(', ')
   end
 end
