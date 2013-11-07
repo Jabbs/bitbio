dda4a5a9165f44a8fb9cb0f64717ceafb8e8736e
Peter Jabbour <petejabbour1@gmail.com>
2013-11-07 09:29:31 -0600

    validate contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :user_type
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
