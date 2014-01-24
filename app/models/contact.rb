b785f8abd8d455e7849d5e002e08faa3b74d9a8a
Peter Jabbour <petejabbour1@gmail.com>
2014-01-24 13:08:09 -0600

    terms and privacy

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,8 @@ class Contact < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   
+  CONTACTS_FROM_LEADS_LIST = ["artem.khlebnikov@bioaster.com", "pietro.piffanelli@tecnoparco.org", "dk@waksman.rutgers.edu", "palma.leopoldo@gmail.com", "ysl0928@gmail.com", "bhampton@som.umaryland.edu", "rvoelker@uoregon.edu", "nvthuyne@gmail.com", "a.ferrero@acgen.es", "jeff@yunes.us", "dtorti26@gmail.com", "jharness@gmail.com"]
+  
   def full_name
     self.first_name + " " + self.last_name
   end
